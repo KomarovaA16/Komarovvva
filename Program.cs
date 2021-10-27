76345d3e3baefedf22c499cbbbdd7bde3c977199
Комарова Александра <91051712+KomarovaA16@users.noreply.github.com>
2021-10-27 19:08:54 +0500

    Add files via upload
    дз номер 1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace kmrvvv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Вот: слышится — а слов не слышу,");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Вот: близится — и тьмится вдруг…");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Но знаю, с поля — или свыше —");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Тот звук — из сердца ли тот звук…");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            
+
+
+
+
+
+
+        }
+    }
+}
