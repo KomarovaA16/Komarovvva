3e3a51ef66784ae680f1252144a4e573d2288390
Комарова Александра <91051712+KomarovaA16@users.noreply.github.com>
2021-10-27 20:10:05 +0500

    Add files via upload
    задание 2

--- Program.cs
+++ Program.cs
@@ -1,29 +1,21 @@
 ﻿using System;
 
-namespace kmrvvv
+namespace KомароваЗадание2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Вот: слышится — а слов не слышу,");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Вот: близится — и тьмится вдруг…");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Но знаю, с поля — или свыше —");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Тот звук — из сердца ли тот звук…");
-
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            
-
-
-
-
-
+            var base1 = double.Parse(Console.ReadLine());
+            var base2 = double.Parse(Console.ReadLine());
+            var heightSide = double.Parse(Console.ReadLine());
+            double sideC = Math.Sqrt(Math.Pow(heightSide,2)+(Math.Pow(base1-base2,2))/4);
+            double trP = base1 + base2 + 2 * sideC;
+            Console.WriteLine($"Периметр трапеции - {trP}");
+            Console.ReadLine();
+            double trPl = (base1 + base2) / 2 * heightSide;
+            Console.WriteLine($"Площадь трапеции - {trPl}");
+            Console.ReadLine();
 
         }
     }
